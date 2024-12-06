bcfb443e100aa6d08da9bd210d95d0c39db01c65
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2024-12-06 03:55:40 +0000

    new changes

--- src/Components/Payroll/Payroll.js
+++ src/Components/Payroll/Payroll.js
@@ -524,7 +524,7 @@ const PayAttendance = ({att, curAtt, setDebtDue, setShortages, sales,
                 if (record.employeeId === curEmployee.i_d){
                     if (att.month === months[new Date(sale.postingDate).getMonth()]){
                         var thisDebt = Number(record.debt) - Number(record.debtRecovered)
-                        saleDebt = Number(saleDebt) + thisDebt 
+                        saleDebt = Number(saleDebt) + (thisDebt>0?thisDebt:0) 
                         saleShortage = Number(saleShortage)+Number(record.shortage) + (Number(thisDebt) < 0 ? thisDebt : 0)
                     }
                 }
