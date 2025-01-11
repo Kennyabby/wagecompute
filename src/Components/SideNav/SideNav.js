64dab3bac96d327845fc0e62c9edd2b5a0954550
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2025-01-11 23:59:29 +0100

    new update

--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -51,6 +51,7 @@ const SideNav = ()=>{
                 </ul>
                 <ul className='navbarr'>
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('dashboard')) && <li name="dashboard" className={curPath==='dashboard'?'selected':''}>Dashboard</li>}
+                    {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('reports')) && <li name="reports" className={curPath==='reports'?'selected':''}>Reports</li>}
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('employees')) && <li name="employees" className={curPath==='employees'?'selected':''}>Employees</li>}
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('departments')) && <li name="departments" className={curPath==='departments'?'selected':''}>Departments</li>}
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('positions')) && <li name="positions" className={curPath==='positions'?'selected':''}>Positions</li>}
@@ -59,7 +60,6 @@ const SideNav = ()=>{
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('sales')) && <li name="sales" className={curPath==='sales'?'selected':''}>Sales</li>}
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('purchase')) && <li name="purchase" className={curPath==='purchase'?'selected':''}>Direct Purchase</li>}
                     {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('expenses')) && <li name="expenses" className={curPath==='expenses'?'selected':''}>Admin Expenses</li>}
-                    {(companyRecord?.status === 'admin' || companyRecord?.permissions.includes('reports')) && <li name="reports" className={curPath==='reports'?'selected':''}>Reports</li>}
                     {(companyRecord?.status === 'admin') && <li name="settings" className={curPath==='settings'?'selected':''}>Settings</li>}
                     <div
                         onClick={logout}
