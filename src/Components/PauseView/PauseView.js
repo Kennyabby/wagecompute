971ae461e7e2c677c3ae13cf7a3c955d52aade82
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2025-05-15 16:25:19 +0100

    new update

--- src/Components/PauseView/PauseView.js
+++ src/Components/PauseView/PauseView.js
@@ -34,7 +34,7 @@ const PauseView = ()=>{
                     autoComplete={false}                                   
                 />
                 <label>
-                    { viewAccess === null ? '' : 'This deployment is temporarily paused'}
+                    { viewAccess === null ? '405 ERROR COULD NO SERVER RESPONSE' : 'This deployment is temporarily paused'}
                 </label>
                 {viewAccess !==null && <div className='pause-base-code'>{`cpt1 : : ${generateCode(5)}-${generateCode(13)}-d${generateCode(5)}aaef${generateCode(2)}`}</div>}
             </div>
