f30cce957e52e3fe71d0f721beca25304bc9f509
Kennyabby <67015671+Kennyabby@users.noreply.github.com>
2025-06-19 10:00:49 +0100

    new update

--- src/Components/Sales/Sales.js
+++ src/Components/Sales/Sales.js
@@ -3020,6 +3020,7 @@ const AddProduct = ({
             }
         }
     }
+
     return (
         <>
             <div className='addproduct'>
@@ -3043,7 +3044,7 @@ const AddProduct = ({
                             })                        
                         }
                         <div className='slprwh-cover-txt'>{`Remaining (${(Number(totalSalesAmount) - Math.abs(Number(totalAmount))).toLocaleString()}) Out Of ${(Number(totalSalesAmount)).toLocaleString()}`}</div>
-                        {(isProductView || (!curSale.approval?.message && curSale.approval !== undefined)) && <div
+                        {(isProductView && (!curSale.approval?.message && curSale.approval !== undefined)) && <div
                             className='slprwh-print'
                             onClick={()=>{
                                 resetSalesEntries()
