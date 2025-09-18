86df987b84b9d559e90c1703bc05990bfa5d52f5
Kennyabby <kennyabby22@gmail.com>
2025-09-18 10:28:47 +0100

    new update

--- src/Components/Accommodation/Accommodation.js
+++ src/Components/Accommodation/Accommodation.js
@@ -104,7 +104,7 @@ const Accommodation = ()=>{
         'moniepoint1':'MP1-8198068382', 'moniepoint2':'MP2-5342270174', 
         'moniepoint3':'MP3-5399647958', 'moniepoint4':'MP4-5536588063', 
         'cash':'CASH', 'Employee':'EMPLOYEE'
-    }
+    } 
 
     const [accommodationFields, setAccommodationFields] = useState({...defaultAccommodationFields})
     const [customerFields, setCustomerFields] = useState({
