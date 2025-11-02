05a2540c2f97e83fcae34e03a7ce922a895140cf
Kennyabby <kennyabby22@gmail.com>
2025-11-02 21:09:18 +0100

    new update

--- src/Components/Sales/SalesReport/SalesReport.js
+++ src/Components/Sales/SalesReport/SalesReport.js
@@ -199,7 +199,7 @@ const SalesReport = ({
             filename:     `SALES REPORT - FROM ${getDate(fromDate)} TO ${getDate(toDate)}.pdf`,
             image:        { type: 'jpeg', quality: 0.98 },
             html2canvas:  { scale: 2 },
-            jsPDF:        { unit: 'in', format: 'A4', orientation: 'portrait' }
+            jsPDF:        { unit: 'in', format: 'A4', orientation: 'landscape' }
         };
         html2pdf().set(options).from(element).save();
     };
