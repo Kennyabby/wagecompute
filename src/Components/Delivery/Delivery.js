93a2d6bffec290a2512516fe9688cde1cf96378c
Kennyabby <kennyabby22@gmail.com>
2025-12-07 22:56:51 +0100

    new update

--- src/Components/Delivery/Delivery.js
+++ src/Components/Delivery/Delivery.js
@@ -275,6 +275,7 @@ const Delivery = () => {
     useEffect(()=>{
         var cmp_val = window.localStorage.getItem('sessn-cmp')        
         getProducts(cmp_val)
+        fetchTables(cmp_val)
         const intervalId = setInterval(()=>{
             if (cmp_val){
                 // Fetch tables
